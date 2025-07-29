394f33694be5f536a7c1a018cc4d8ddbfb8a3ae3
Edward <estavick23@gmail.com>
2025-07-28 19:55:44 -0700

    initial commit with local files

--- Chapter 2/hello_world.py
+++ Chapter 2/hello_world.py
@@ -1,6 +1,14 @@
+<<<<<<< HEAD
 message = "Hello Python World!"
 print(message)
 
 message = "Hello Python Crash Course World!"
 print(message)
+=======
+message = "Hello Python World!"
+print(message)
+
+message = "Hello Python Crash Course World!"
+print(message)
+>>>>>>> 8ddfee9e799cf7e0c52235d7cc032fe96f3b0f9a
 print()
---
+++ README.md
@@ -0,0 +1,1 @@
+Python projects for Python Crash Course, 3rd Edition: A Hands-On, Project-Based Introduction to Programming by Eric Matthes
