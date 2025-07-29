9c9c7737419c317f7c62a23a982d6f4dff75c987
Edward <estavick23@gmail.com>
2025-07-28 19:59:05 -0700

    test duplicate .py file

--- Chapter 2/hello_world.py
+++ Chapter 2/hello_world.py
@@ -1,9 +1,17 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 message = "Hello Python World!"
 print(message)
 
 message = "Hello Python Crash Course World!"
 print(message)
+=======
+message = "Hello Python World!"
+print(message)
+
+message = "Hello Python Crash Course World!"
+print(message)
+>>>>>>> 8ddfee9e799cf7e0c52235d7cc032fe96f3b0f9a
 =======
 message = "Hello Python World!"
 print(message)
