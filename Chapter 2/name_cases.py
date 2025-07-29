ad8e461a336407f357d32f50325e2a2b7569a435
Edward Stavick <edwardstavick@edward-mbp.local>
2025-07-29 09:19:55 -0700

    removed markets on .py files

--- Chapter 2/name_cases.py
+++ Chapter 2/name_cases.py
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 name = "edward stavick"
 message = f"Hello {name.title()}, would you like to learn Python today?"
 print(message)
@@ -27,7 +24,7 @@ print(name.lstrip())
 print(name.rstrip())
 
 filename = "\tpython_notes.txt"
-=======
+
 name = "edward stavick"
 message = f"Hello {name.title()}, would you like to learn Python today?"
 print(message)
@@ -54,8 +51,7 @@ print(name.lstrip())
 print(name.rstrip())
 
 filename = "\tpython_notes.txt"
->>>>>>> 8ddfee9e799cf7e0c52235d7cc032fe96f3b0f9a
-=======
+
 name = "edward stavick"
 message = f"Hello {name.title()}, would you like to learn Python today?"
 print(message)
@@ -82,33 +78,4 @@ print(name.lstrip())
 print(name.rstrip())
 
 filename = "\tpython_notes.txt"
->>>>>>> 8ddfee9e799cf7e0c52235d7cc032fe96f3b0f9a
-=======
-name = "edward stavick"
-message = f"Hello {name.title()}, would you like to learn Python today?"
-print(message)
-
-name = "edward stavick"
-message = f"Hello {name.lower()}, would you like to learn Python today?"
-print(message)
-
-name = "edward stavick"
-message = f"Hello {name.upper()}, would you like to learn Python today?"
-print(message)
-
-print('Albert Einstein once said, "A person who never made a mistake never tried anything new."')
-
-famous_person = "Albert Einstein"
-message = f'{famous_person} once said, "A person who never made a mistake never tried anything new."'
-print(message)
-
-name = "\nCallen "
-print(name)
-name = "\t Callen "
-print(name.strip())
-print(name.lstrip())
-print(name.rstrip())
-
-filename = "\tpython_notes.txt"
->>>>>>> 8ddfee9e799cf7e0c52235d7cc032fe96f3b0f9a
 print(filename.removesuffix('.txt'))
