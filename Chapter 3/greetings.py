18df2e7e92abac70795cefaf327f3fa59052a500
Edward <estavick23@gmail.com>
2025-07-30 16:30:20 -0700

    added manufacturer_names.py

--- Chapter 3/greetings.py
+++ Chapter 3/greetings.py
@@ -1,6 +1,6 @@
 # Prints out messages to my friends
-friend_name = ['ryan', 'joe', 'rj']
+friend_names = ['ryan', 'joe', 'rj']
 
-print(f"How are you {friend_name[0].title()}?")
-print(f"How are you {friend_name[1].title()}?")
-print(f"How are you {friend_name[2].title()}?")
+print(f"How are you {friend_names[0].title()}?")
+print(f"How are you {friend_names[1].title()}?")
+print(f"How are you {friend_names[2].title()}?")
---
+++ Chapter 3/manufacturer_names.py
@@ -0,0 +1,7 @@
+# Prints out all my favorite manufacturers and facts
+manufacturer_names = ['honda', 'tesla','toyota', 'yamaha']
+
+print(f"I have a 2020 {manufacturer_names[0].title()}.")
+print(f"I want to own a car made by {manufacturer_names[1].title()}.")
+print(f"I want to own a {manufacturer_names[3].title()} motorcycle.")
+print(f"My brother has a {manufacturer_names[2].title()} RAV4.")
